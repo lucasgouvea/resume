06d99ddcfb86ad258092bf8f548599ee012c1348
Lucas Gouvea <lucas.gouvea@casaamerica.com.br>
2019-03-05 10:52:46 -0300

    resume update

--- src/Components/Body/body.jsx
+++ src/Components/Body/body.jsx
@@ -15,14 +15,14 @@ class Body extends Component {
               <span className="text-primary">Gouvea</span>
             </h1>
             <div className="subheading mb-5">
-              141 Rua Nilo Máximo · Jacareí, SP · (12) 99616-4013 ·
+              309 R. PREF. TIGRE MAIA · ITAJUBÁ, MG · (12) 99616-4013 ·
               <a href="mailto:name@email.com">lucasgouvea93@gmail.com</a>
             </div>
             <p className="lead mb-5">
               About to graduate Computer Engineer, seeking new challenges and
               passionate for Software Development involving new technologies.
-              Capable of learn fast new frameworks. Self-taught, aiming to
-              become a software engineer.
+              Capable of learn new frameworks fast. Self-taught, aiming to
+              become a Software Engineer.
             </p>
             <div className="social-icons">
               <a href="https://www.linkedin.com/in/lucas-gouvea-56a288139/">
@@ -46,7 +46,23 @@ class Body extends Component {
             <div className="resume-item d-flex flex-column flex-md-row mb-5">
               <div className="resume-content mr-auto">
                 <h3 className="mb-0">Software Development Internship</h3>
-                <div className="subheading mb-3">FITec</div>
+                <div className="subheading mb-3">Hivelabs - Itajubá</div>
+                <p>Fullstack mobile developer, using React Native & NodeJS.</p>
+                <p>
+                  Responsible for bringing new technologies such as Docker into
+                  the development process.
+                </p>
+                <p />
+              </div>
+              <div className="resume-date text-md-right">
+                <span className="text-primary">February 2019 - Present</span>
+              </div>
+            </div>
+
+            <div className="resume-item d-flex flex-column flex-md-row mb-5">
+              <div className="resume-content mr-auto">
+                <h3 className="mb-0">Software Development Internship</h3>
+                <div className="subheading mb-3">FITec - SJC</div>
                 <p>
                   Worked on Ericsson's BSCS billing & rating system, developing
                   features and writing automated tests.
@@ -58,7 +74,7 @@ class Body extends Component {
                 <p />
               </div>
               <div className="resume-date text-md-right">
-                <span className="text-primary">July 2018 - Present</span>
+                <span className="text-primary">July 2018 - January 2019</span>
               </div>
             </div>
           </div>
@@ -104,11 +120,25 @@ class Body extends Component {
               </li>
               <li>
                 <i className="fa-li fa fa-check" />
-                Javascript: NodeJS, AngularJS, ReactJS, NPM
+                Javascript: NodeJS, AngularJS, ReactJS
               </li>
               <li>
                 <i className="fa-li fa fa-check" />
-                Tools: Git, Maven, Jenkins
+                Android: React Native
+              </li>
+
+              <li>
+                <i className="fa-li fa fa-check" />
+                Tools: Git, Maven, Docker, Jenkins
+              </li>
+              <li>
+                <i className="fa-li fa fa-check" />
+                Capable of quick deploying web apps on AWS using Node + React +
+                Mongo
+              </li>
+              <li>
+                <i className="fa-li fa fa-check" />
+                Capable of handling UNIX based systems
               </li>
             </ul>
             <div className="subheading mb-3" />
@@ -122,8 +152,13 @@ class Body extends Component {
         >
           <div className="my-auto">
             <h2 className="mb-5">Interests</h2>
-            <p>?</p>
-            <p className="mb-0">?</p>
+            <p className="mb-0">
+              Strongly interested in developing and architecting systems based
+              on the state of the art technologies. Besides that, I enjoy
+              reading books on themes such as psychology, philosophy, and
+              neuroscience. Cyberpunk enthusiast, lover of sci-fi series and
+              retrogaming.
+            </p>
           </div>
         </section>
       </div>
